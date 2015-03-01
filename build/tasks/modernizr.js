6d7b2e97ff1146c4862f94feff0d9dfbd0525df9
Raymond Patterson <enterprise@Enterprise.local>
2015-02-28 17:24:02 -0800

    Fixed issue with glob execution.

--- build/tasks/modernizr.js
+++ build/tasks/modernizr.js
@@ -9,12 +9,14 @@ module.exports = function(gulp, $, cfg) {
   var task_cfg = cfg.tasks.modernizr;
   var common_src = '*.js';
   var common_dir = path.join(cfg.dest, cfg.dir.common);
-  var common_filename = glob.sync(common_src, {
-    cwd: common_dir
-  })[0];
 
   gulp.task('modernizr', function() {
 
+    var common_filename = glob.sync(common_src, {
+      cwd: common_dir
+    })[0];
+
+    console.log('common_filename', common_filename);
     var commmon = gulp.src(common_src, {
       cwd: common_dir
     });
@@ -23,6 +25,7 @@ module.exports = function(gulp, $, cfg) {
         cwd: task_cfg.cwd
       })
       .pipe($.modernizr('not-writing-to-file-system', task_cfg.settings));
+
 
     return merge(modernizr, commmon)
       .pipe($.concat(common_filename))
